ddfe84cf94c9bb5e7acab53620eaa5d57b67f11e
ichubtou <msjj0809@naver.com>
2023-10-14 05:52:55 +0900

    [Fix] JobPosting 엔티티 오타 수정

--- src/main/java/com/wanted/wanted/jobposting/entity/JobPosting.java
+++ src/main/java/com/wanted/wanted/jobposting/entity/JobPosting.java
@@ -1,6 +1,7 @@
 package com.wanted.wanted.jobposting.entity;
 
 import com.wanted.wanted.company.entity.Company;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -8,12 +9,13 @@ import java.util.List;
 
 @Entity
 @Getter
+@Builder
 public class JobPosting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long posting_id;
 
-    private String Position;
+    private String position;
 
     private Integer reward;
 
