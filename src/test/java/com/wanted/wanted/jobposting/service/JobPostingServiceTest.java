3755620f20399491829800c8d257b47b5f53cf8e
ichubtou <msjj0809@naver.com>
2023-10-14 12:15:52 +0900

    [Test] JobPosting 검색어 조회 기능 Unit Test

--- src/test/java/com/wanted/wanted/jobposting/service/JobPostingServiceTest.java
+++ src/test/java/com/wanted/wanted/jobposting/service/JobPostingServiceTest.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest
 public class JobPostingServiceTest {
@@ -181,6 +182,50 @@ public class JobPostingServiceTest {
 
         // when
         List<JobPostingDto.GetResponse> jobPostingList = jobPostingService.getJobPostings();
+
+        // then
+        assertThat(jobPostingList.get(0).getPosting_id()).isEqualTo(position_id);
+        assertThat(jobPostingList.get(0).getCompany_name()).isEqualTo(company_name);
+        assertThat(jobPostingList.get(0).getCountry()).isEqualTo(country);
+        assertThat(jobPostingList.get(0).getLocation()).isEqualTo(location);
+        assertThat(jobPostingList.get(0).getPosition()).isEqualTo(position);
+        assertThat(jobPostingList.get(0).getReward()).isEqualTo(reward);
+        assertThat(jobPostingList.get(0).getSkill()).isEqualTo(skill);
+    }
+
+    @Test
+    @DisplayName("JobPosting 검색어 조회")
+    public void testGetSearchJobPosting() {
+        // given
+        Long position_id = 1L;
+        String position = "백엔드 주니어 개발자";
+        Integer reward = 1000000;
+        String description = "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..";
+        String skill = "Python";
+        Long company_id = 1L;
+        String company_name = "원티드랩";
+        String country = "한국";
+        String location = "서울";
+
+        JobPosting jobPosting = new JobPosting();
+        jobPosting.setPosting_id(position_id);
+        jobPosting.setPosition(position);
+        jobPosting.setReward(reward);
+        jobPosting.setDescription(description);
+        jobPosting.setSkill(skill);
+
+        Company company = new Company();
+        company.setCompany_id(company_id);
+        company.setCompany_name(company_name);
+        company.setCountry(country);
+        company.setLocation(location);
+
+        jobPosting.setCompany(company);
+
+        Mockito.when(jobPostingRepository.searchJobPostings(anyString())).thenReturn(List.of(jobPosting));
+
+        // when
+        List<JobPostingDto.GetResponse> jobPostingList = jobPostingService.getSearchJobPostings("서울");
 
         // then
         assertThat(jobPostingList.get(0).getPosting_id()).isEqualTo(position_id);
