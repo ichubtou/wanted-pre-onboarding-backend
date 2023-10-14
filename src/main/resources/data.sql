96c827de813f42c958af40b771d2357db9a7b3df
ichubtou <msjj0809@naver.com>
2023-10-14 09:39:57 +0900

    [Feat] 초기 데이터 자동 로딩 구현

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO applicant(applicant_id) VALUES (1) , (2), (3);
+
+INSERT INTO company(company_id, company_name, country, location) VALUES (1, "원티드랩", "한국", "서울"),
+                                                                        (2, "원티드코리아", "한국", "부산"),
+                                                                        (3, "네이버", "한국", "판교"),
+                                                                        (4, "카카오", "한국", "판교");
