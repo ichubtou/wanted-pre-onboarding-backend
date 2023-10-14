c349af266ceae8cba4e9727d84a9b16c508e3b50
ichubtou <msjj0809@naver.com>
2023-10-14 10:09:08 +0900

    [Test] JobPosting 수정 기능 Unit Test

--- src/test/java/com/wanted/wanted/jobposting/service/JobPostingServiceTest.java
+++ src/test/java/com/wanted/wanted/jobposting/service/JobPostingServiceTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 @SpringBootTest
 public class JobPostingServiceTest {
@@ -61,7 +64,62 @@ public class JobPostingServiceTest {
         Mockito.when(jobPostingRepository.save(Mockito.any(JobPosting.class))).thenReturn(jobPosting);
 
         // when
-        JobPostingDto.PostResponse result = jobPostingService.createJobPosting(jobPostingDto);
+        JobPostingDto.PostAndUpdateResponse result = jobPostingService.createJobPosting(jobPostingDto);
+
+        // then
+        assertThat(result.getPosting_id()).isEqualTo(position_id);
+        assertThat(result.getCompany_id()).isEqualTo(company_id);
+        assertThat(result.getCompany_name()).isEqualTo(company_name);
+        assertThat(result.getCountry()).isEqualTo(country);
+        assertThat(result.getLocation()).isEqualTo(location);
+        assertThat(result.getPosition()).isEqualTo(position);
+        assertThat(result.getReward()).isEqualTo(reward);
+        assertThat(result.getDescription()).isEqualTo(description);
+        assertThat(result.getSkill()).isEqualTo(skill);
+    }
+
+    @Test
+    @DisplayName("JobPosting 수정")
+    public void testUpdateJobPosting() {
+        // given
+        Long position_id = 1L;
+        String position = "백엔드 주니어 개발자";
+        Integer reward = 1000000;
+        String description = "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..";
+        String skill = "Python";
+        Long company_id = 1L;
+        String company_name = "원티드랩";
+        String country = "한국";
+        String location = "서울";
+
+        Integer updateReward = 1000000;
+        String updateSkill = "Django";
+
+        JobPostingDto.Update jobPostingDto = new JobPostingDto.Update();
+        jobPostingDto.setPosition(position);
+        jobPostingDto.setReward(updateReward);
+        jobPostingDto.setDescription(description);
+        jobPostingDto.setSkill(updateSkill);
+
+        JobPosting jobPosting = new JobPosting();
+        jobPosting.setPosting_id(position_id);
+        jobPosting.setPosition(position);
+        jobPosting.setReward(reward);
+        jobPosting.setDescription(description);
+        jobPosting.setSkill(skill);
+
+        Company company = new Company();
+        company.setCompany_id(company_id);
+        company.setCompany_name(company_name);
+        company.setCountry(country);
+        company.setLocation(location);
+
+        jobPosting.setCompany(company);
+
+        Mockito.when(jobPostingRepository.findById(anyLong())).thenReturn(Optional.of(jobPosting));
+
+        // when
+        JobPostingDto.PostAndUpdateResponse result = jobPostingService.updateJobPosting(position_id ,jobPostingDto);
 
         // then
         assertThat(result.getPosting_id()).isEqualTo(position_id);
